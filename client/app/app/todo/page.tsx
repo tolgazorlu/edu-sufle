6388de14673a29d4bb8f83c5d98165564fd90bc0
Tolga Zorlu <tolgazorlu@Mac.home>
2025-05-03 17:21:23 +0300

    usecontext error fixed

--- client/app/app/todo/page.tsx
+++ client/app/app/todo/page.tsx
@@ -5,6 +5,7 @@ import { AppSidebar } from '@/components/app-sidebar';
 import { SidebarProvider } from '@/components/ui/sidebar';
 import { SiteHeader } from '@/components/site-header';
 import Todo from '@/components/Todo';
+import React from 'react';
 
 export default function TodoPage() {
   const handleConnect = (address: string) => {
@@ -20,19 +21,21 @@ export default function TodoPage() {
   };
 
   return (
-    <SidebarProvider>
-      <AppSidebar variant="inset" />
-      <SidebarInset>
-        <SiteHeader 
-          title="Learning Path Todo" 
-          handleConnect={handleConnect} 
-          handleDisconnect={handleDisconnect} 
-          handleBalanceUpdate={handleBalanceUpdate} 
-        />
-        <div className="flex flex-col h-full bg-gray-50">
-          <Todo />
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
+    <div>
+      <SidebarProvider>
+        <AppSidebar variant="inset" />
+        <SidebarInset>
+          <SiteHeader 
+            title="Learning Path Todo" 
+            handleConnect={handleConnect} 
+            handleDisconnect={handleDisconnect} 
+            handleBalanceUpdate={handleBalanceUpdate} 
+          />
+          <div className="flex flex-col h-full bg-gray-50">
+            <Todo />
+          </div>
+        </SidebarInset>
+      </SidebarProvider>
+    </div>
   );
 } 
